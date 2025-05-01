6aded20a5ee77c7941369cb3d27c1d5217c3e35c
lvitorinogoncalves <lvitorinogoncalves@gmail.com>
2025-05-01 11:18:24 -0300

    Ajute opção de menu listar músicas.

--- Menus/MenuMostrarArtistas.cs
+++ Menus/MenuMostrarArtistas.cs
@@ -5,7 +5,7 @@ namespace ScreenSoundDB.Menus;
 
 internal class MenuMostrarArtistas : Menu
 {
-    public void Executar(DAL<Artista> artistaDal)
+    public override void Executar(DAL<Artista> artistaDal)
     {
         base.Executar(artistaDal);
         ExibirTituloDaOpcao("Exibindo todos os artistas registradas na nossa aplicação");
